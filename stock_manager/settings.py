76b970738177bd89823b2e83029ca0c0192be66d
kelvinmacharia254 <pilotkelvin0@gmail.com>
2024-10-03 12:34:33 +0300

    add CustomUserModel

--- stock_manager/settings.py
+++ stock_manager/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 3rd party
+    'rest_framework',
+    'django_extensions',
+    # internals
+    "stock.apps.StockConfig",
+    "users.apps.UsersConfig"
 ]
 
 MIDDLEWARE = [
@@ -121,3 +127,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.CustomUser'
