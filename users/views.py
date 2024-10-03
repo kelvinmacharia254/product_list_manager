a40c5b053856148f7bc473c083de13e7e2efa7c2
kelvinmacharia254 <pilotkelvin0@gmail.com>
2024-10-03 13:50:27 +0300

    add userslist endpoint

--- users/views.py
+++ users/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import generics
+
+from users.models import CustomUser
+from users.serilizers import CustomUserSerializer
+
 
 # Create your views here.
+
+
+class UsersListView(generics.ListAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
